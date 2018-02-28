2ada09bbe982063b96ffea01de46f5744a798dfe
Isaac Blandin Mac <myung0966@gmail.com>
2018-02-28 11:19:43 -0600

    added example hardware initialization into the opmode template

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTemplate.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTemplate.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by isaac.blandin on 2/26/18.
@@ -11,16 +12,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @Disabled
 public class TeleOpTemplate extends LinearOpMode {
     //declare hardware here
-
+    //DcMotor rightDrive;
+    //DcMotor leftDrive;
 
     @Override public void runOpMode() {
         //initialize hardware here
-
+        //rightDrive = hardwareMap.dcMotor.get("right");
 
         waitForStart();
         while(opModeIsActive()){
             //main TeleOp loop here
-
 
         }
         stop();
