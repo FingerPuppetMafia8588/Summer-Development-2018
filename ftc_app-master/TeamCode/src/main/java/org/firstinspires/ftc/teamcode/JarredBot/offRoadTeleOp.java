b406f9966271a486b87e698b88cd56d1a0852b78
Isaac Blandin Mac <myung0966@gmail.com>
2018-03-28 11:27:04 -0500

    added rudementary code for drive and auto level for offroad chassis.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JarredBot/offRoadTeleOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JarredBot/offRoadTeleOp.java
@@ -13,7 +13,57 @@ public class offRoadTeleOp extends offRoadHardware{
 
         while (opModeIsActive()) {
 
+            //grabs input for two joystick arcade tank drive
+            xDrive = gamepad1.right_stick_x;
+            yDrive = -gamepad1.left_stick_y;
+
+            //equation for drive and set power
+            rightFrontDrive.setPower(yDrive - xDrive);
+            rightBackDrive.setPower(yDrive - xDrive);
+            leftFrontDrive.setPower(yDrive + xDrive);
+            leftBackDrive.setPower(yDrive + xDrive);
+
+
+            xOffset = lastAngles.thirdAngle;
+            yOffset = lastAngles.secondAngle;
+
+            xPower = xOffset/75 + 0.2;
+            yPower = yOffset/75 + 0.2;
+
+            rfVAr = yPower + xPower;
+            rbVar = -yPower - xPower;
+            lfVar = yPower - xPower;
+            lbVar = -yPower + xPower;
+
+            max = rfVAr;
+            if (rbVar > max){
+                max = rbVar;
+            }
+            if (lfVar > max){
+                max = lfVar;
+            }
+            if (lbVar > max){
+                max = lbVar;
+            }
+
+            rfVAr = rfVAr/max;
+            rbVar = rbVar/max;
+            lfVar = lfVar/max;
+            lbVar = lbVar/max;
+
+            if (Math.abs(xOffset) > 8 || Math.abs(yOffset) > 8){
+                rightFrontElevation.setPower(rfVAr);
+                rightBackElevation.setPower(rbVar);
+                leftFrontElevation.setPower(lfVar);
+                leftBackElevation.setPower(lbVar);
+            } else {
+                rightFrontElevation.setPower(0);
+                rightBackElevation.setPower(0);
+                leftFrontElevation.setPower(0);
+                leftBackElevation.setPower(0);
+            }
 
         }
+        stop();
     }
 }
