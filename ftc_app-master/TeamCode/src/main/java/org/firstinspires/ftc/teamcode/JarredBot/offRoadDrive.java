d51c4d7dd8d3b683746eba77cf97ea42aeddfe00
Isaac Blandin Mac <myung0966@gmail.com>
2018-05-14 22:43:02 -0500

    added the ability for the robot to self balance the robot main core

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JarredBot/offRoadDrive.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JarredBot/offRoadDrive.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.teamcode.JarredBot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
 /**
  * Created by isaac.blandin on 4/17/18.
  */
@@ -12,6 +16,10 @@ public class offRoadDrive extends offRoadHardware{
         initOffRoad();
 
         double holdpower = 0;
+
+        xOffset = 0;
+        yOffset = 0;
+        elevation = 0;
 
         rightFrontElevation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBackElevation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -32,6 +40,8 @@ public class offRoadDrive extends offRoadHardware{
             xDrive = gamepad1.right_stick_x;
             yDrive = -gamepad1.left_stick_y;
 
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
             //equation for drive and set power
             rightFrontDrive.setPower(yDrive - xDrive);
             rightBackDrive.setPower(yDrive - xDrive);
@@ -44,15 +54,47 @@ public class offRoadDrive extends offRoadHardware{
             rightBackElevation.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             leftBackElevation.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-            rightFrontElevation.setTargetPosition(0);
-            rightBackElevation.setTargetPosition(0);
-            leftFrontElevation.setTargetPosition(0);
-            leftBackElevation.setTargetPosition(0);
+            if(gamepad1.b){
+                elevation += 2;
+            }
+            if (gamepad1.a){
+                elevation -= 2;
+            }
 
-            rightFrontElevation.setPower(0.3);
-            rightBackElevation.setPower(0.3);
-            leftFrontElevation.setPower(0.3);
-            leftBackElevation.setPower(0.3);
+            if (gamepad1.dpad_up|| angles.thirdAngle > 5){
+                yOffset += 2;
+            }
+            if (gamepad1.dpad_down|| angles.thirdAngle < -5){
+                yOffset -= 2;
+            }
+            if (gamepad1.dpad_right || angles.secondAngle < -5){
+                xOffset -= 2;
+            }
+            if (gamepad1.dpad_left || angles.secondAngle > 5){
+                xOffset += 2;
+            }
+
+
+
+            rightFrontElevation.setTargetPosition(0 - xOffset - yOffset + elevation);
+            rightBackElevation.setTargetPosition(0 + xOffset - yOffset - elevation);
+            leftFrontElevation.setTargetPosition(0 + xOffset - yOffset + elevation);
+            leftBackElevation.setTargetPosition(0  - xOffset - yOffset - elevation);
+
+            rightFrontElevation.setPower(0.6);
+            rightBackElevation.setPower(0.6);
+            leftFrontElevation.setPower(0.6);
+            leftBackElevation.setPower(0.6);
+
+            telemetry.addData("xOffset", -xOffset);
+            telemetry.addData("yoffset", yOffset);
+            telemetry.addData("elevationOffset", -elevation);
+
+            telemetry.addData("heading", angles.firstAngle);
+            telemetry.addData("y-axis", angles.secondAngle);
+            telemetry.addData("x-axis", angles.thirdAngle);
+
+            telemetry.update();
 
         }
         stop();
